54a8ac9b0842298c73899330f00503ef27cd676d
merivasil92 <merivasil92@gmail.com>
2017-05-30 21:46:37 +0300

    Create vktest.py

---
+++ vktest.py
@@ -0,0 +1,9 @@
+import vk
+
+session = vk.Session()
+api = vk.API(session)
+user = api.users.get(user_ids=1)[0]
+print user['first_name']
+print user['last_name']
+print user['uid']
+print api.users.getFollowers(user_id=user['uid'])
