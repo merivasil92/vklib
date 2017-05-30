847b20d71d726a83b5c21bc868258ba6345f7314
merivasil92 <merivasil92gmail.com>
2017-05-30 22:24:36 +0300

    test3

--- vktest.py
+++ vktest.py
@@ -5,6 +5,6 @@ api = vk.API(session)
 user = api.users.get(user_ids=1)[0]
 print user['first_name']
 print user['last_name']
-print user['uid']
+
 followers = api.users.getFollowers(user_id=user['uid'])
 print followers
